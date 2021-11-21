3715f7c0ddf117942ea0d7ccc7d3664d150619a7
Abdullah Kavaklı <abdullahkavakli@hotmail.com>
2021-11-21 14:30:08 +0300

    forms was handled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_094836) do
+ActiveRecord::Schema.define(version: 2021_11_21_102352) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_094836) do
     t.text "paragraph"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
