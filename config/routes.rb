f325aff88beacceb94167f2cda1c33e15a78ab6b
Abdullah Kavaklı <abdullahkavakli@hotmail.com>
2021-11-21 15:22:22 +0300

    index changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
-  #get 'home/index'
-  root 'home#index'
+  get 'home/index'
+  #root 'home#index'
+  root 'articles#index'#üsttekiyle değiştirmek gerekiyor trending articles yapıldığı zaman
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
